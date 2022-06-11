8f1ad0720f23760e0dcefaa82de462e676a48cc7
Leon van der Grient <leon@devtastic.co>
2022-06-11 09:16:11 +0200

    fix: fix export to html command

--- cli/commands/export.ts
+++ cli/commands/export.ts
@@ -1,20 +1,30 @@
 import ora from "ora";
 import shelljs from "shelljs";
 import path from "path";
+import fs from "fs-extra";
+import cpy from "cpy";
 
 export default async function exportCommand() {
   const spinner = ora(`Exporting your project...`);
+  spinner.start();
 
   const projectDir = process.cwd();
   const useYarn = Boolean(shelljs.which("yarn"));
 
   shelljs.cd(path.resolve(projectDir, ".polemic", "parchment"));
   shelljs.exec(
-    `${useYarn ? "yarn" : "npm"} run export`,
+    `PROJECT_DIR=${projectDir} ${useYarn ? "yarn" : "npm"} run export`,
     {
       silent: true,
     },
     async () => {
+      const buildDir = path.resolve(projectDir, ".polemic", "parchment", "out");
+      const exportDir = path.resolve(projectDir, "export", "html");
+
+      await fs.remove(exportDir);
+      await cpy(`${buildDir}/**`, exportDir, {
+        overwrite: true,
+      });
       spinner.succeed();
     }
   );
