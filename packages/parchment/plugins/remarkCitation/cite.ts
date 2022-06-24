3aa9ab067f99881e3608308b6798a0dffbe9ff59
Leon van der Grient <leon@devtastic.co>
2022-06-24 13:42:11 +0200

    fix: remove plugins from parchment

--- packages/parchment/plugins/remarkCitation/cite.ts
+++
@@ -1,5 +0,0 @@
-import Cite from "citation-js";
-
-export const toCSL = (data: string): any => {
-  return new Cite(data, { generateGraph: false });
-};
--- packages/parchment/plugins/remarkCitation/regex.ts
+++
@@ -1,4 +0,0 @@
-export const citeExtractorRe =
-  /\[([^[\]]*@[^[\]]+)]|(?!\b)(@[a-zA-Z0-9_][a-zA-Z0-9_:.#$%&\-+?<>~]*)/g;
-export const citeKeyRe = /@([a-zA-Z0-9_][a-zA-Z\d_:.#$%&\-+?<>~]*)/;
-export const citeBracketRe = /\[.*]/;
